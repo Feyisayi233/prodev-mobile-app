42890289c343dbe4bbf5c90c9d6c3f3c23977528
Feyisayo <feyisayo233@gmail.com>
2025-09-11 16:03:16 +0100

    Create _join.ts

---
+++ prodev-mobile-app-0x04/styles/_join.ts
@@ -0,0 +1,12 @@
+// You can define your join screen styles here
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  // Example style
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
+
+export { styles };
